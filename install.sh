72d6e7e3c89b701b7bffc9b44bffe0a6c3aa3b49
unknown <jens.lorden@imagimob.com>
2020-05-11 13:59:12 +0200

    Started workingon install script

---
+++ install.sh
@@ -0,0 +1,8 @@
+sudo apt-get update -y
+sudo apt-get install i3
+sudo apt-get install i3lock
+sudo apt-get install i3status
+sudo apt install compton
+sudo apt-get install rofi
+sudo apt-get install -y fonts-font-awesome 
+sudo apt-get install -y feh
